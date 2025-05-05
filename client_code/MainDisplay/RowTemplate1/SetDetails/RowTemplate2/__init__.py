bd60fb88a79ff81486ef240058514189bb4a4d2a
aarondeno11@gmail.com <aarondeno11@gmail.com>
2025-05-05 04:45:23 +0000

    Edited MainDisplay.RowTemplate1.SetDetails.RowTemplate2 and settings

--- client_code/MainDisplay/RowTemplate1/SetDetails/RowTemplate2/__init__.py
+++ client_code/MainDisplay/RowTemplate1/SetDetails/RowTemplate2/__init__.py
@@ -1,4 +1,5 @@
 from ._anvil_designer import RowTemplate2Template
+from anvil_extras import augment
 from anvil import *
 
 
@@ -6,5 +7,8 @@ class RowTemplate2(RowTemplate2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    augment.set_event_handler(self, 'click', self.row_click)
 
+  def row_click(self, **event_args):
+    print("row clicked")
     # Any code you write here will run before the form opens.
