33a5aefd731322a21339a9c5c9b5fab7ad2c919a
aarondeno11@gmail.com <aarondeno11@gmail.com>
2025-09-22 00:51:09 +0000

    Edited MainDisplay and AddStdWorkoutForm

--- client_code/AddStdWorkoutForm/__init__.py
+++ client_code/AddStdWorkoutForm/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import AddStdWorkoutFormTemplate
 from anvil import *
+import anvil
 import anvil.server
 
 
@@ -16,7 +17,7 @@ class AddStdWorkoutForm(AddStdWorkoutFormTemplate):
     self.md.add_exercise_button_click(body_part='Chest', exercise='DB Incline Fly')
     self.md.add_exercise_button_click(body_part='Arms', exercise='DB Skull Crushers')
     self.md.add_exercise_button_click(body_part='Arms', exercise='Cable Tricep Push')
-    self.close()
+    anvil.close_alert()
 
   def add_pull_1_button_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -24,7 +25,7 @@ class AddStdWorkoutForm(AddStdWorkoutFormTemplate):
     self.md.add_exercise_button_click(body_part='Back', exercise='Narrow Seated Row')
     self.md.add_exercise_button_click(body_part='Arms', exercise='Hammer Curl')
     self.md.add_exercise_button_click(body_part='Back', exercise='Seated Reverse DB Fly')
-    self.close()
+    anvil.close_alert()
 
   def add_legs_1_button_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -32,7 +33,7 @@ class AddStdWorkoutForm(AddStdWorkoutFormTemplate):
     self.md.add_exercise_button_click(body_part='Hamstrings', exercise='DB Romanian Deadlift')
     self.md.add_exercise_button_click(body_part='Core', exercise='Plank')
     self.md.add_exercise_button_click(body_part='Core', exercise='DB Suitcase Carry Uneven')
-    self.close()
+    anvil.close_alert()
 
   def add_push_2_button_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -40,13 +41,15 @@ class AddStdWorkoutForm(AddStdWorkoutFormTemplate):
     self.md.add_exercise_button_click(body_part='Chest', exercise='DB Decline Press')
     self.md.add_exercise_button_click(body_part='Shoulders', exercise='Shoulder Press')
     self.md.add_exercise_button_click(body_part='Shoulders', exercise='DB Front/Side Raises')
-    self.close()
+    anvil.close_alert()
+    
   def add_pull_2_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.md.add_exercise_button_click(body_part='Back', exercise='Bent Row')
     self.md.add_exercise_button_click(body_part='Back', exercise='Overhead DB Pullover')
     self.md.add_exercise_button_click(body_part='Arms', exercise='Cable One-Arm Curl')
     self.md.add_exercise_button_click(body_part='Shoulders', exercise='Wide Grip BB Upright Row')
+    anvil.close_alert()
 
   def add_legs_2_button_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -54,4 +57,4 @@ class AddStdWorkoutForm(AddStdWorkoutFormTemplate):
     self.md.add_exercise_button_click(body_part='Calves', exercise='DB Single Calf Raise')
     self.md.add_exercise_button_click(body_part='Core', exercise='Side Plank')
     self.md.add_exercise_button_click(body_part='Core', exercise='Leg Lifts')
-
+    anvil.close_alert()
